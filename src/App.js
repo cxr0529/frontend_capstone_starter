9f98a82e81fa177a488e25b11095afa90ca840f9
Chuck R <cxr0529@gmail.com>
2024-06-27 15:19:55 -0400

    added reports

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import InstantConsultation from './Components/InstantConsultation/instantconsult
 import BookingConsultation from './Components/bookingConsultation.js';
 import Notification from './Components/Notification/notification.js';
 import ReviewForm from './Components/ReviewForm/reviewform.js';
+import ProfileForm from './Components/ProfileCard/profilecard.js';
+import ReportLayout from './Components/ReportLayout/reportlayout.js';
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
                 <Route path="instantconsult" element={<InstantConsultation/>}/>
                 <Route path='booking' element={<BookingConsultation/>}/>
                 <Route path='reviews' element={<ReviewForm/>}/>
+                <Route path='profile' element={<ProfileForm/>}/>
+                <Route path='report' element={<ReportLayout/>}/>
               </Routes>
             </Notification>
         </BrowserRouter>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,13 +4,12 @@ import "./Navbar.css";
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
-
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [username, setUsername] = useState("");
-    const[email,setEmail]=useState("");
+    const [email, setEmail] = useState("");
     const [showDropdown, setShowDropdown] = useState(false);
     const handleClick = () => setClick(!click);
- 
+
     const handleLogout = () => {
         sessionStorage.removeItem("auth-token");
         sessionStorage.removeItem("name");
@@ -20,81 +19,105 @@ const Navbar = () => {
         localStorage.removeItem("doctorData");
         setIsLoggedIn(false);
         // setUsername("");
-       
+
         // Remove the reviewFormData from local storage
         for (let i = 0; i < localStorage.length; i++) {
-          const key = localStorage.key(i);
-          if (key.startsWith("reviewFormData_")) {
-            localStorage.removeItem(key);
-          }
+            const key = localStorage.key(i);
+            if (key.startsWith("reviewFormData_")) {
+                localStorage.removeItem(key);
+            }
         }
         setEmail('');
         window.location.reload();
     }
     const handleDropdown = () => {
-      setShowDropdown(!showDropdown);
+        setShowDropdown(!showDropdown);
     }
-    useEffect(() => { 
-      const storedemail = sessionStorage.getItem("email");
+    useEffect(() => {
+        const storedemail = sessionStorage.getItem("email");
 
-      if (storedemail) {
+        if (storedemail) {
             setIsLoggedIn(true);
             setUsername(storedemail);
-          }
-        }, []);
-  return (
-    <nav>
-      <div className="nav__logo">
-        <Link to="/">
-        StayHealthy <i style={{color:'#2190FF'}} className="fa fa-user-md"></i></Link>
-        <span>.</span>
-      </div>
-      <div className="nav__icon" onClick={handleClick}>
-        <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
-      </div>
-      <ul className={click ? 'nav__links active' : 'nav__links'}>
-        <li className="link">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="link">
-            <Link to="/instantconsult">Instant Consultation</Link>
-        </li>
-        <li className="link">
-          <Link to="/booking">Appointments</Link>
-        </li>
-        <li className="link">
-          <Link to="/healthblog">Health Blog</Link>
-        </li>
-        <li className="link">
-         <Link to="/reviews">Reviews</Link>
-        </li>
-        {isLoggedIn?(
-          <>
-            <li className="link">
-              <button className="btn2" onClick={handleLogout}>
-                Logout
-              </button>
-            </li>
-            
-          </>
-        ) : (
-          <>
-            <li className="link">
-              <Link to="/signup">
-                <button className="btn1">Sign Up</button>
-              </Link>
-            </li>
-            <li className="link">
-              <Link to="/login">
-                <button className="btn1">Login</button>
-              </Link>
-            </li>
-          </>
-        )}
-      </ul>
-    </nav>
-  );
+        }
+    }, []);
+
+    useEffect(() => {
+        // Check if the user is already logged in
+        const storedUsername = sessionStorage.getItem("name");
+
+        if (storedUsername) {
+            setIsLoggedIn(true);
+            setUsername(storedUsername);
+        }
+    }, []);
+    return (
+        <nav>
+            <div className="nav__logo">
+                <Link to="/">
+                    StayHealthy <i style={{ color: '#2190FF' }} className="fa fa-user-md"></i></Link>
+                <span>.</span>
+            </div>
+            <div className="nav__icon" onClick={handleClick}>
+                <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+            </div>
+            <ul className={click ? 'nav__links active' : 'nav__links'}>
+                <li className="link">
+                    <Link to="/">Home</Link>
+                </li>
+                <li className="link">
+                    <Link to="/booking">Appointments</Link>
+                </li>
+                <li className="link">
+                    <Link to="/healthblog">Health Blog</Link>
+                </li>
+                <li className="link">
+                    <Link to="/instantconsult">Instant Consultation</Link>
+                </li>
+                <li className="link">
+                    <Link to="/reviews">Reviews</Link>
+                </li>
+                {isLoggedIn ? (
+                    <>
+                        <li onClick={handleDropdown} className="link welcome-user">
+                            <p>
+                                Welcome, {username}
+                            </p>
+                            {showDropdown && (
+                                <ul className="dropdown-menu">
+                                    <li>
+                                        <Link to="/profile">Your Profile</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/report">Your Reports</Link>
+                                    </li>
+                                </ul>
+                            )}
+                        </li>
+                        <li className="link">
+                            <button className="btn2" onClick={handleLogout}>
+                                Logout
+                            </button>
+                        </li>
+
+                    </>
+                ) : (
+                    <>
+                        <li className="link">
+                            <Link to="/signup">
+                                <button className="btn1">Sign Up</button>
+                            </Link>
+                        </li>
+                        <li className="link">
+                            <Link to="/login">
+                                <button className="btn1">Login</button>
+                            </Link>
+                        </li>
+                    </>
+                )}
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
-
